a11172a35328ec4fe78fe5dee167e7733c8a8fe3
Lida <chornalida8@gmail.com>
2025-04-22 22:59:44 +0300

    feat: added more

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from './components/pages/Homescreen'
+import More from './components/pages/more/More'
 
 function App(){
     return(<div>
         <Home />
+        <More />
     </div>
     )
 }
